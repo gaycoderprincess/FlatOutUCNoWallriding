063378b0a668d20df1fa8eb7e87e0bf967db4305
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-01-12 14:33:55 +0100

    use new version check

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <windows.h>
 #include "nya_commonhooklib.h"
+#include "fo2versioncheck.h"
 
 BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
 	switch( fdwReason ) {
 		case DLL_PROCESS_ATTACH: {
-			if (NyaHookLib::GetEntryPoint() != 0x24CEF7) {
-				MessageBoxA(nullptr, "Unsupported game version! Make sure you're using the Steam GFWL version (.exe size of 4242504 bytes)", "nya?!~", MB_ICONERROR);
-				exit(0);
-				return TRUE;
-			}
-
+			DoFlatOutVersionCheck(FO2Version::FOUC_GFWL);
 			NyaHookLib::PatchRelative(NyaHookLib::JMP, 0x42CBDC, 0x42D19B);
 		} break;
 		default:
